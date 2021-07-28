fd05e0ed5357081241397cce9b23834d66bea807
Jonathan <bowe.jonathan@yahoo.com>
2021-07-28 13:58:59 -0400

    Basic Routing Implementation
    Added four pages/components (home, characters, about, and contact),
    added them to app-routing, changed nav-bar to be links to said pages
    (including making logo clickable to send to home page) and added some
    basic CSS to make sure page content actually showed on site.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './navigation/about/about.component';
+import { HomeComponent } from './navigation/home/home.component';
+import { CharactersComponent } from './navigation/characters/characters.component';
+import { ContactComponent } from './navigation/contact/contact.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'characters', component: CharactersComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: '', pathMatch: 'full', redirectTo: 'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
